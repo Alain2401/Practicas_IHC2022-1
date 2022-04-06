8f089eaac61364543da9b2f9e4241efcaba5f0ba
Alain2401 <a1273699@uabc.edu.mx>
2022-04-06 11:18:24 -0700

    Practica4

---
+++ Practica3/Assets/Samples/Google Cardboard XR Plugin for Unity/1.14.0/Hello Cardboard/Scripts/VRLocomotion.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRLocomotion : GazeInteractable
+{
+    public Vector3 playerHeightOffset = new Vector3(0, 1.6f, 0);
+
+    public override void OnPointerEnter()
+    {
+        base.OnPointerEnter();
+    }
+
+    public override void OnPointerExit()
+    {
+        base.OnPointerExit();
+    }
+
+    public override void OnPointerClick()
+    {
+        base.OnPointerClick();
+        //Teletransportar jugador
+        Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+        player.position = transform.position + playerHeightOffset;
+    }
+}
